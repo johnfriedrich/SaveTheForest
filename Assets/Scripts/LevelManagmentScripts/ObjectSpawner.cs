7a32af430e0cdae2f5aa5e48c30b9d0d30d31d90
Leon <leonkuhn@web.de>
2020-01-31 22:39:29 +0100

    [Added] Fire+AnimalSpawner

---
+++ Assets/Scripts/LevelManagmentScripts/ObjectSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    TreeScript[] trees;
+    [SerializeField]
+    float animalSpawnTime;
+    [SerializeField]
+    float fireSpawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trees = GetComponentsInChildren<TreeScript>();
+        StartCoroutine(SpawnAnimal());
+        StartCoroutine(SpawnFire());
+    }
+
+
+    IEnumerator SpawnFire()
+    {
+        trees[Random.Range(0, trees.Length)].StartFire();
+        yield return new WaitForSeconds(fireSpawnTime);
+        StartCoroutine(SpawnFire());
+    }
+
+    IEnumerator SpawnAnimal()
+    {
+        trees[Random.Range(0, trees.Length)].SpawnAnimal();
+        yield return new WaitForSeconds(animalSpawnTime);
+        StartCoroutine(SpawnAnimal());
+    }
+}
