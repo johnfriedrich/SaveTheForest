3fa2cb69da57b3fb0a8b643527fa16fa4cdefeb5
Patrick Rotter <p.rotter@wt.de>
2020-01-31 23:13:11 +0100

    [MODIFIED] PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,28 +3,69 @@
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    //public float speed = 0;
+    [Header("Player Settings")]
+    public float speed = 0;
+
+    private CharacterController _cc;
+    private Camera _playerCam;
+
+    [Header("Camera Settings")]
+    public float minimumX = -90.0f;
+    public float maximumX = 90.0f;
+    public float cameraSmoothTime = 20.0f;
+
+    private void Awake()
+    {
+        _cc = GetComponent<CharacterController>();
+        _playerCam = GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        RotateCamera();
+        Move();
+        Interact();
+    }
     
-    //[Header("Camera Settings")]
-    //public float MinimumX = -90.0f;
-    //public float MaximumX = 90.0f;
-    //public float CameraSmoothTime = 20.0f;
+    private void RotateCamera()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * cameraSmoothTime * Time.deltaTime;
+        float vertical = Input.GetAxis("Mouse Y") * cameraSmoothTime * Time.deltaTime;
 
-    //private void Start()
-    //{
-    //}
+        transform.localRotation *= Quaternion.Euler(0.0f, horizontal, 0.0f);
+        transform.localRotation *= Quaternion.Euler(-vertical, 0.0f, 0.0f);
+        transform.localRotation = ClampRotationAroundXAxis(transform.localRotation);
+    }
+    
+    private void Move()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+        movement *= speed * Time.deltaTime;
 
-    //private void Update()
-    //{
-    //}
+        _cc.SimpleMove(movement);
+    }
+    
+    private void Interact()
+    {
+        
+    }
 
-    //private void RotateCamera() //Mouse rotation is used for first-person view only
-    //{
-    //    float horizontal = Input.GetAxis("Mouse X") * CameraSmoothTime * Time.deltaTime;
-    //    float vertical = Input.GetAxis("Mouse Y") * CameraSmoothTime * Time.deltaTime;
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
 
-    //    transform.localRotation *= Quaternion.Euler(0.0f, horizontal, 0.0f);
-    //    FirstPerson.transform.localRotation *= Quaternion.Euler(-vertical, 0.0f, 0.0f);
-    //    FirstPerson.transform.localRotation = ClampRotationAroundXAxis(FirstPerson.transform.localRotation);
-    //}
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, minimumX, maximumX);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
 }
