c6e51de4c2f62773d8372c3a29ea94088be7d99b
Patrick Rotter <p.rotter@wt.de>
2020-02-01 12:09:40 +0100

    [Added] Change burnt tree to stump

---
+++ Assets/Scripts/ObjectScripts/BurnTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace ObjectScripts
+{
+    public class BurnTree : MonoBehaviour
+    {
+        [SerializeField] private float burnDuration = 60.0f;
+        [SerializeField] private TreeScript treeScript;
+        [SerializeField] private GameObject stumpPrefab;
+
+        private float timer;
+
+        private void Update()
+        {
+            if (!treeScript.HasFire)
+                timer = 0.0f;
+            else if (treeScript.HasFire)
+                timer += Time.deltaTime;
+
+            if (timer >= burnDuration)
+                SwitchTreeToStump();
+        }
+
+        private void SwitchTreeToStump()
+        {
+            GameObject stump = Instantiate(stumpPrefab, transform.position, Quaternion.identity);
+            stump.name = "Stump";
+            
+            Destroy(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            timer = 0.0f;
+            treeScript.HasAnimal = false;
+            treeScript.HasFire = false;
+        }
+    }
+}
