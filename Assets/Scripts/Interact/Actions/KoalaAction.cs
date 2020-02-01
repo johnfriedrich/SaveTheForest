37335c2d7281601c73f7a0f23b72a85bcf0ef8d6
John <jonathanfriedrich@live.de>
2020-02-02 00:09:10 +0100

    Add Koala

--- Assets/Scripts/Interact/Actions/KoalaAction.cs
+++ Assets/Scripts/Interact/Actions/KoalaAction.cs
@@ -1,10 +1,20 @@
-﻿namespace Interact.Actions
+﻿using UnityEngine;
+
+namespace Interact.Actions
 {
     public class KoalaAction : UsableAction
     {
+        [SerializeField] private Animator _animator;
+
         public override void Use()
         {
             base.Use();
+            _animator.SetBool("wave", true);
+        }
+
+        public void Sit()
+        {
+            _animator.SetBool("wave", false);
         }
     }
 }
