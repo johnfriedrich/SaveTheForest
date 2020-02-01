b688125d3a4ffad09d24d6d27b44ef81796ef3bb
John <jonathanfriedrich@live.de>
2020-02-01 12:30:53 +0100

    Use new treescript for burning

--- Assets/Scripts/ObjectScripts/BurnTree.cs
+++ Assets/Scripts/ObjectScripts/BurnTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Interact;
 using UnityEngine;
 
 namespace ObjectScripts
@@ -6,16 +7,21 @@ namespace ObjectScripts
     public class BurnTree : MonoBehaviour
     {
         [SerializeField] private float burnDuration = 60.0f;
-        [SerializeField] private TreeScript treeScript;
+        [SerializeField] private UsableObject treeScript;
         [SerializeField] private GameObject stumpPrefab;
 
         private float timer;
 
+        private void Start()
+        {
+            stumpPrefab.SetActive(false);
+        }
+
         private void Update()
         {
-            if (!treeScript.HasFire)
+            if (!treeScript.IsOnFire)
                 timer = 0.0f;
-            else if (treeScript.HasFire)
+            else if (treeScript.IsOnFire)
                 timer += Time.deltaTime;
 
             if (timer >= burnDuration)
@@ -24,17 +30,11 @@ namespace ObjectScripts
 
         private void SwitchTreeToStump()
         {
-            GameObject stump = Instantiate(stumpPrefab, transform.position, Quaternion.identity);
-            stump.name = "Stump";
+            stumpPrefab.SetActive(true);
+            stumpPrefab.transform.SetParent(null);
+            stumpPrefab.name = "Stump";
             
             Destroy(gameObject);
-        }
-
-        private void OnDestroy()
-        {
-            timer = 0.0f;
-            treeScript.HasAnimal = false;
-            treeScript.HasFire = false;
         }
     }
 }
