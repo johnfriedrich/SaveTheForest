1714dc81b3b044a28f527a05d644aacb909399a9
Leon <leonkuhn@web.de>
2020-02-01 10:56:59 +0100

    [Added] LevelManager

---
+++ Assets/Scripts/LevelManagementScripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Problem { Fire, Animal};
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+    int[] _problems =  new int[2];
+
+    public int Animals { get => _problems[1]; }
+    public int Fires { get => _problems[0];  }
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    public void ProblemSpawned(Problem problem)
+    {
+        _problems[(int)problem]++;
+    }
+
+    public void ProblemSolved(Problem problem)
+    {
+        _problems[(int)problem]--;
+    }
+
+}
