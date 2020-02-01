2fa5aa43ae16ad11c0f239b56becea988ff5530c
John <jonathanfriedrich@live.de>
2020-02-01 17:18:50 +0100

    Basic sound support for Grabables

--- Assets/Scripts/Interact/Grabable.cs
+++ Assets/Scripts/Interact/Grabable.cs
@@ -3,8 +3,39 @@ using UnityEngine;
 
 namespace Interact
 {
-    public class Grabable : MonoBehaviour {
+    public class Grabable : MonoBehaviour
+    {
+        private float timer;
+        [SerializeField] private float _cooldown = 10f;
 
+        private AudioSource _audioSource;
+
+        private void Start()
+        {
+            _audioSource = gameObject.AddComponent<AudioSource>();
+            _audioSource.playOnAwake = false;
+        }
+
+        private void Update()
+        {
+            if (timer < _cooldown)
+            {
+                timer += Time.deltaTime;
+                
+            }
+            else
+            {
+                if (AudioClip != null)
+                {
+                    _audioSource.PlayOneShot(AudioClip);
+                }
+
+                timer = 0;
+            }
+        }
+
+        public AudioClip AudioClip;
+        
         public InteractableEnum Type;
 
         public InteractableEnum RequiredType;
