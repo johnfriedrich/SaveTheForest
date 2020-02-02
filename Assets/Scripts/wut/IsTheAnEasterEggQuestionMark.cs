1961ab45ee7c57f4b40bc31dcd27fdc41c56712b
Leon <leonkuhn@web.de>
2020-02-02 14:22:09 +0100

    EasterEgg

---
+++ Assets/Scripts/wut/IsTheAnEasterEggQuestionMark.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsTheAnEasterEggQuestionMark : MonoBehaviour
+{
+    [SerializeField]
+    GameObject treeMesh;
+    [SerializeField]
+    GameObject cannon;
+    BoxCollider boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player") && Input.GetButtonDown("Fire2"))
+        {
+            Magic();
+        }
+    }
+
+    private void Magic()
+    {
+        treeMesh.SetActive(false);
+        cannon.SetActive(true);
+        boxCollider.enabled = true;
+    }
+}
